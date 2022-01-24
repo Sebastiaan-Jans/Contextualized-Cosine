3724ee564ee582749288c191c315d166bae51c99
dasjadeleeuw <91197515+dasjadeleeuw@users.noreply.github.com>
2022-01-24 17:02:51 +0100

    Update mahalanobis.py

--- mahalanobis.py
+++ mahalanobis.py
@@ -4,7 +4,8 @@ from collections import defaultdict
 import pandas as pd
 import re
 from scipy.spatial import distance
-
+import scipy as sp
+from matplotlib import pyplot as plt
 
 
 def reduced_glove_reps(words, path):
@@ -77,15 +78,17 @@ def get_words():
 
 def compute_mahalanobis(cat, representations, human_judgements):
     '''
-    Function that computes the Mahalanobis distance between each word pair in each category.
+    Function that gets the Mahalanobis distance between each word pair in each category.
     Returns (1) a vector containing the Mahalanobis distance between each word pair and
     (2) a vector containing the human judgement similarity score for each word pair.
     '''
     m_sims = [] #similarity between words based on mahalanobis distance
     h_sims = [] #similarity between words based on human judgements
 
-    #cosine similarity
-    cos = torch.nn.CosineSimilarity(dim=0, eps=1e-6)
+    cov = np.cov(get_cat_matrix(representations, cat))
+    #TODO: check if det of matrix is 0
+    #inv_cov = sp.linalg.inv(cov)
+    inv_cov = np.linalg.pinv(cov)
 
     #get the wordpair
     for wordpair in human_judgements[cat]:
@@ -99,10 +102,7 @@ def compute_mahalanobis(cat, representations, human_judgements):
                 if w1[0] == word1 and w2[0] == word2:
 
                     #compute the mahalanobis distance between the found glove representations and append this to the new vector
-                    #TODO: implement the Mahalanobis distance
-
-                    #mahalanobis = distance.mahalanobis(w1[1], w2[1], inverse_covariance_matrix)
-                    mahalanobis = cos(w1[1], w2[1]) #this should be the mahalanobis distance!!
+                    mahalanobis = sp.spatial.distance.mahalanobis(w1[1], w2[1], inv_cov)
                     m_sims.append(mahalanobis)
 
         #append the human similarity score to the new vector
@@ -111,20 +111,84 @@ def compute_mahalanobis(cat, representations, human_judgements):
     return m_sims, h_sims
 
 
+def get_cat_matrix(representations, cat):
+    '''
+    Function that retrieves the glove embeddings and returns these
+    in a matrix that is transposed.
+    '''
+    matrix = []
 
-# get the words, human judgements and glove representations
-words, human_judgements = get_words()
-representations = reduced_glove_reps(words, 'glove_dim25.txt')
+    for (_, v) in representations[cat]:
+        matrix.append(v.numpy())
 
-mahalanobis_sims = defaultdict(list)
-human_sims = defaultdict(list)
+    return np.array(matrix).T
 
-for cat in words:
-    # get word similarity based on (1) mahalanobis distance and (2) human judgements represented as vectors
-    mahala, human = compute_mahalanobis(cat, representations, human_judgements)
-    mahalanobis_sims[cat] = mahala
-    human_sims[cat] = human
 
-print(mahalanobis_sims)
-print(human_sims)
+def run_all_categories(filename):
+    '''
+    Function that retrieves the words, glove representations and human
+    judgements. Calls compute_mahalanobis to compute the Mahalanobis
+    distance between all word pairs and returns this in a dictionary
+    along with a dictionary containing the human judgements.
+    '''
+
+    # get the words, human judgements and glove representations
+    words, human_judgements = get_words()
+    representations = reduced_glove_reps(words, filename)
+
+    mahalanobis_sims = defaultdict(list)
+    human_sims = defaultdict(list)
+
+    for cat in words:
+        # get word similarity based on (1) mahalanobis distance and (2) human judgements represented as vectors
+        mahala, human = compute_mahalanobis(cat, representations, human_judgements)
+        mahalanobis_sims[cat] = mahala
+        human_sims[cat] = human
+
+    return words, mahalanobis_sims, human_sims
+
+
+def compute_cos_sim(mahalanobis_sims, human_sims, words):
+    '''
+    Function that computes cosine similarity between mahalanobis
+    distance similarities and human judgements per category.
+    '''
+    cos_sim_per_cat = defaultdict(int)
+
+    for cat in words:
+        cos = np.dot(mahalanobis_sims[cat], human_sims[cat])/(np.linalg.norm(mahalanobis_sims[cat])*np.linalg.norm(human_sims[cat]))
+        cos_sim_per_cat[cat] = cos
+
+    return cos_sim_per_cat
+
+
+def plot_results(mahalanobis_sims, human_sims, words):
+    '''
+    Function that plots the results of comparing the Mahalanobis
+    distances between word pairs with the human judgements.
+    '''
+    #compute the highest Mahalanobis distance to standardize the distances
+    max_mahala = max([max(mahalanobis_sims[cat]) for cat in mahalanobis_sims])
+
+    for cat in words:
+        #standardize Mahalanobis distances
+        normalized_mahala = np.divide(mahalanobis_sims[cat], max_mahala) #TODO: or divide by 7
+        #divide each Mahalanobis distance by the corresponding human judgement
+        compare = [i / j for i, j in zip(normalized_mahala, human_sims[cat])]
+
+        #plot the results
+        plt.plot(sorted(compare))
+
+    plt.legend(words.keys())
+    plt.show()
+
+
+words, mahalanobis_distances, human_judgements = run_all_categories('glove_dim25.txt')
+print(mahalanobis_distances)
+print(human_judgements)
+
+cos_sim = compute_cos_sim(mahalanobis_distances, human_judgements, words)
+print(cos_sim)
+
+plot_results(mahalanobis_distances, human_judgements, words)
 
